9085646cedf7e40a035775a8e843a727399956ca
rserota <raphael.serota@gmail.com>
2015-05-27 20:34:38 -0600

    week one

--- week1/main.js
+++ week1/main.js
@@ -51,16 +51,16 @@
 // foo('yup', 'ok');
 
 
-var add = function(){
-    console.log(arguments.length);
+// var add = function(){
+//     console.log(arguments.length);
 
-};
+// };
 
-add(5) // return 5
+// add(5) // return 5
 
-add(10,3) // return 13
+// add(10,3) // return 13
 
-add(1,2,3,10,10,10) // should return 36
+// add(1,2,3,10,10,10) // should return 36
 
 
 
@@ -68,3 +68,47 @@ add(1,2,3,10,10,10) // should return 36
 // 1: 'b',
 // 2: 'c',
 // length: number
+
+// 'name' : 'Steve',
+// 'age'  : 12
+
+// var addTwo = function(number){
+//     return number + 2
+// }
+
+// var addFive = function(number){
+//     return number + 5
+// }
+
+// var makeAddFunction = function(amount){
+
+//     var add = function(number){
+//         return number + amount
+//     }
+//     return add
+// }
+
+// var addTwo = makeAddFunction(2)
+
+// var seven = addTwo(5)
+
+// ['a','b','c']
+
+var greet = function(){ console.log('hi!')}
+var person = {
+    name : 'steve',
+    age  : 12,
+    sayHi : greet,
+    favoriteAnimals : ['badger', 'lion'],
+    familyMembers : {
+        mom : 'Janet',
+        dad : 'Jeff',
+    }
+};
+
+for ( var key in person ) {
+    // console.log(key) // first iteration: key == 'name'
+    console.log(person[key]) // person['name']
+
+}
+
